60777aeb32bc19c49ff9908fb566f4ae8a3e2675
Anuj Verma <anujsonist01@gmail.com>
2025-10-03 00:09:18 +0530

    fix: change div to anchor for scroll link in HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -27,10 +27,10 @@ const HeroSection = () => {
                 </div>
             </div>
 
-            <div className='absolute bottom-8 left-1/2 transform-translate-x-1/2 flex flex-col items-center animate-bounce'>
+            <a href='#about' className='absolute bottom-8 left-1/2 transform-translate-x-1/2 flex flex-col items-center animate-bounce'>
                 <span className='text-sm text-muted-foreground mb-2' >Scroll</span>
                 <ArrowDown className='h-5 w-5 text-primary' />
-            </div>
+            </a>
         </section>
     );
 }
